0998ff66aec69d5b40315c3a7f7b2ec776fb892d
Jossie McManus <jossie.mcmanus@gmail.com>
2020-03-08 12:29:40 -0600

    added functions for chirps

--- app/controllers/chirps_controller.rb
+++ app/controllers/chirps_controller.rb
@@ -2,4 +2,48 @@ class ChirpsController < ApplicationController
   def index
     @chirps = Chirp.all
   end
+
+  def show
+    @chirp = Chirp.find(params[:id])
+  end
+
+  def new
+    @chirp = Chirp.new
+  end
+
+  def create
+    @chirp = Chirp.new(chirp_params)
+
+    if @chirp.save
+      redirect_to @chirp
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @chirp = Chirp.find(params[:id])
+  end
+
+  def update
+    @chirp = Chirp.find(params[:id])
+
+    if @chirp.update(chirp_params)
+      redirect_to @chirp
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @chirp = Chirp.find(params[:id])
+    @chirp.destroy
+
+    redirect_to chirps_path
+  end
+
+  private
+  def chirp_params
+    params.require(:chirp).permit(:text)
+  end
 end
--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -1,2 +1,4 @@
 class Chirp < ApplicationRecord
+  validates :text, presence: true,
+                   length: {minimum: 5}
 end
